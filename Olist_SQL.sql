ceee35c0af58bc8ef6051c2ad997c55fb993c9a7
Ngoc Bich <bichptn04@gmail.com>
2025-10-03 11:47:02 +0700

    Add SQL script for Olist project

--- Olist_SQL.sql
+++ Olist_SQL.sql
@@ -1,2 +1,15 @@
+SELECT 
+    C.customer_unique_id,
+    DATEDIFF(DAY, MAX(O.order_purchase_timestamp), CAST('2018-09-01' AS DATE)) AS Recency,
+    COUNT(DISTINCT O.order_id) AS Frequency,
+    SUM(I.price + I.freight_value) AS Monetary
+FROM dbo.olist_customers_dataset C
+JOIN dbo.olist_orders_dataset O
+    ON C.customer_id = O.customer_id
+JOIN dbo.olist_order_items_dataset I
+    ON I.order_id = O.order_id
+WHERE O.order_status = 'delivered'
+GROUP BY C.customer_unique_id;
+
 SELECT *
 FROM [dbo].[Customer_RFM]
